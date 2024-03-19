e86e64aa7d89bd2ba58ec0657db3179219b5accb
Kyle P. Ulman <kylepulman@gmail.com>
2024-03-19 01:57:23 -0400

    feat: add homepage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const page = {
+		title: 'Kyle P. Ulman'
+	};
+</script>
+
+<h1>{page.title}</h1>
+<p>Hello! I'm Kyle. This sentence describes my identity as a developer.</p>
+<p>
+	This is the professional work I'm currently engaged in. I could also list projects I'm working on
+	or companies I've founded here.
+</p>
+<p>
+	This paragraph details what drives me as a developer. It could be a passion for solving problems,
+	manifesting ideas, or working with teams toward larger goals.
+</p>
+<p>
+	Here, I'll describe some of the things I like to do outside of dev. If applicable, I'll include a
+	link to a social media feed or blog that shows me engaging in said hobbies. If I'm feeling
+	especially social, I'll include a call-to-action to those who share interests with me to reach
+	out!
+</p>
+<ul>
+	<h2>Find me on:</h2>
+	<li><a href="https://github.com/kylepulman">GitHub</a></li>
+	<li><a href="https://youtube.com/@kylepulman">YouTube</a></li>
+	<li><a href="https://www.upwork.com/freelancers/~016325a1e595bfe265">Upwork</a></li>
+</ul>
+<h2>Let's Work Together</h2>
+<p>If you think I'd be a good fit for a project, please complete the form below.</p>
+<form method="post">
+	<label for="name">
+		Your Name
+		<input type="text" id="name" name="name" />
+	</label>
+	<label for="email">
+		Your Email
+		<input type="email" id="email" name="email" />
+	</label>
+	<label for="message">
+		Please describe your project and any additional details:
+		<textarea name="message" id="message" cols="30" rows="10"></textarea>
+	</label>
+</form>
